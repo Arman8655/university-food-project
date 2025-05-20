9447d62b7a859c5eb3a06e40936221e7a3842855
Arman8655 <allahyariarman421@gmail.com>
2025-05-20 23:26:42 +0330

    Add files via upload

---
+++ insert_data.sql
@@ -0,0 +1,44 @@
+insert into persons
+values
+    (1, 'ahmad rezaei', '0151170941'),
+    (2,'mojtaba kashani', '0151170942'),
+    (3, 'hosein talebi', '0151170943'),
+    (4, 'ali jaberi', '0151170944'),
+    (5, 'hassan akbari','0151170945');
+
+
+insert into professors
+values
+    (1, 1, '9017001', 'mechanic', 'assistant professor'),
+    (2, 2, '9915002', 'physics', 'associate professor'),
+    (3, 3, '99133003', 'computer science', 'professor');
+
+
+
+insert into students
+values
+    (1,3, '40313001', 'computer science'),
+    (2 , 4 , '40418002', 'polymer'),
+    (3,5, '40311003', 'mathematics' );
+
+
+
+
+insert into menu
+values
+    (1, 'kabab', '50000', '2025-7-8'),
+    (2, 'jooje', '60000', '2025-7-9'),
+    (3, 'koofte', '45000', '2025-7-10'),
+    (4, 'gheime', '80000', '2025-7-10');
+
+
+insert into reservations
+values
+    (1,1, 1, '5'),
+    (2, 2 , 2 , '3'),
+    (3, 3, 4, '9'),
+    (4, 4, 3, '6'),
+    (5, 5, '1', '11');
+
+
+
